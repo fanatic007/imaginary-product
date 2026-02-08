ce3641287160918c6b1b3e5128a9a266f79a3d17
Sarang Tayde <sarang.tayde@gmail.com>
2026-02-09 03:42:55 +0530

    chore: eslint setup for code quality and IDE live linting; path aliases

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,18 +2,34 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tagger from "@dhiwise/component-tagger";
+import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
   // This changes the out put dir from dist to build
   // comment this out if that isn't relevant for your project
   build: {
     outDir: "build",
     chunkSizeWarningLimit: 2000,
   },
-  plugins: [tsconfigPaths(), react(), tagger()],
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    tagger(),
+    eslint({
+      cache: false,
+      include: ['src/**/*.js', 'src/**/*.jsx', 'src/**/*.ts', 'src/**/*.tsx'],
+      exclude: ['node_modules', 'build'],
+      failOnError: true,
+    }),
+  ],
   server: {
-    port: "4028",
+    port: 4028,
     host: "0.0.0.0",
     strictPort: true,
     allowedHosts: ['.amazonaws.com', '.builtwithrocket.new']
